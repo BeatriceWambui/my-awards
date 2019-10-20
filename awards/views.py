9bee1ccacd77033d0ae655f379c9fd565cc608d0
Wachira Beatrice <BeatriceWambui>
2019-10-20 20:31:12 +0300

    create a def for profi;le in views.py

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Profile
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/accounts/register/')
 def index(request):
     return render(request,'blueprint/index.html')
+
+@login_required(login_url='/accounts/register/')
+def profile(request):
+    current_user=request.user
+    post=Image.objects.filter(profile_id=current_user.id)
+    return render(request,'blueprint/profile.html',{'post':post,'forms':form})
+
+    

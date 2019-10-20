a4bee6813a11d66b4cc8ee1411b66e253105f533
Wachira Beatrice <BeatriceWambui>
2019-10-20 17:28:51 +0300

    create a model for profile

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_image = models.ImageField(upload_to='photos/',null=True,default ='photos/default.jpg')
+    bio = models.CharField(max_length=50)        
+    username = models.OneToOneField(User,unique = True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.username.username} Profile'
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+ 

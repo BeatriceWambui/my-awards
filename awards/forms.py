ea85912c79d8cbfacad55e139f945f0078f3dd70
Wachira Beatrice <BeatriceWambui>
2019-10-21 19:27:58 +0300

    create a news letter form the username and email

--- awards/forms.py
+++ awards/forms.py
@@ -1,6 +1,10 @@
 from django import forms
-from .models import Profile
+from .models import Profile,NewsLetterRecipients
 class UploadProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields=['profile_image','bio','contact','projects']
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')

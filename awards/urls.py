9c77c54489d61abbaa195d43700676d2bdfe9e13
Wachira Beatrice <BeatriceWambui>
2019-10-23 16:02:59 +0300

    alll upload,api_-tiken and api/merch urls to url.py files

--- awards/urls.py
+++ awards/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns=[
     url('^$',views.index,name='index'),
@@ -10,7 +11,11 @@ urlpatterns=[
     url(r'^profile/$',views.uploadProfile,name='uploadProfile'),
     url(r'^ajax/newsletter/$', views.newsletter, name='newsletter'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^api/merch/$',views.ProjectList.as_view())
+    url(r'^api/merch/$',views.ProjectList.as_view()),
+    url(r'^upload/$',views.UploadProject,name='UploadProject'),
+    url(r'^api_-token-auth/',obtain_auth_token),
+    url(r'api/merch/merch-id/(?P<pk>[0-9]+)/$',
+        views.MerchDescription.as_view())
     ]
 
 if settings.DEBUG:

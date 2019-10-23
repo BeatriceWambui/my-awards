740c6c1898bfc1a70758eacbb24bfcb582b58343
Wachira Beatrice <BeatriceWambui>
2019-10-23 15:52:55 +0300

    create a project  upload form with fields title,image,description,link and editor

--- awards/forms.py
+++ awards/forms.py
@@ -1,10 +1,15 @@
 from django import forms
-from .models import Profile,NewsLetterRecipients
+from .models import Profile,NewsLetterRecipients,Project
 class UploadProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields=['profile_image','bio','contact','projects']
+        fields=['profile_image','bio','contact','projects','username']
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields =['title','image','description','link','editor']
